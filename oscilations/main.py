6057ef4ce9e3c7d9769e296276a9d1e9304543db
positivcheg94 <positivcheg94@gmail.com>
2016-03-07 21:33:16 +0200

    removed useless tabs ( harmonic and dampind motions are just trivial cases of forced motion )

--- oscilations/main.py
+++ oscilations/main.py
@@ -16,23 +16,28 @@ t_symbol = Symbol('t')
 
 CONST_TIME_SIMULATING = 100
 
-def make_f(f,a):
+
+def make_f(f, a):
     _a = np.array(a)
-    def dec_f(y,t):
-        res = np.roll(y,-1)
+
+    def dec_f(y, t):
+        res = np.roll(y, -1)
         res[-1] = f(t) - _a.dot(y)
         return res
     return dec_f
 
-def zero(*args,**kwargs):
+
+def zero(*args, **kwargs):
     return 0
+
 
 class ParamPicker(QWidget):
 
-    def __init__(self, name, params,mapping, parent=None):
+    def __init__(self, name, params, defaults, mapping, parent=None):
         super().__init__(parent)
         self._params = list(params)
-        self._mapping = mapping
+        self._defaults = list(defaults)
+        self._mapping = list(mapping)
         self._layout = QVBoxLayout(self)
         self._layout.setContentsMargins(0, 0, 0, 0)
         self._layout.setSpacing(0)
@@ -41,16 +46,19 @@ class ParamPicker(QWidget):
         self._layout.addWidget(self._name, 0, Qt.AlignHCenter)
         self._labels = []
         self._edits = []
-        for i in range(0, len(self._params)):
+        for param, default in zip(self._params, self._defaults):
             horizontal_layout = QHBoxLayout()
-            label = QLabel(self._params[i])
+            label = QLabel(param)
             label.setAlignment(Qt.AlignCenter)
             edit = QLineEdit()
+            edit.setText(default)
             edit.setAlignment(Qt.AlignCenter)
             self._labels.append(label)
             self._edits.append(edit)
-            horizontal_layout.addWidget(label, 0, Qt.AlignRight | Qt.AlignHCenter)
-            horizontal_layout.addWidget(edit, 0, Qt.AlignLeft | Qt.AlignHCenter)
+            horizontal_layout.addWidget(
+                label, 0, Qt.AlignRight | Qt.AlignHCenter)
+            horizontal_layout.addWidget(
+                edit, 0, Qt.AlignLeft | Qt.AlignHCenter)
             self._layout.addLayout(horizontal_layout)
         self._layout.addStretch()
 
@@ -67,32 +75,29 @@ class MainWindow(QWidget):
         super().__init__(parent)
 
         self._axis_pen = pg.mkPen('r')
+        self._line_pen = pg.mkPen(color='#0C27D9', width=2)
+        self._plot_background = pg.mkBrush(color='#bfbfbf')
         self._error_box = QErrorMessage()
-        self._t = np.linspace(0,CONST_TIME_SIMULATING,1000)
+        self._t = np.linspace(0, CONST_TIME_SIMULATING, 1000)
 
         self.resize(640, 480)
 
         self._layout = QVBoxLayout(self)
         self._tab_view = QTabWidget()
         self._tab_view.setUsesScrollButtons(False)
-        self._harmonic = ParamPicker(
-            'Harmonic motion', [CONST_OMEGA, CONST_X0, CONST_DX0],
-            [float,float,float])
-        self._damping = ParamPicker(
-            'Damping motion', [CONST_OMEGA, CONST_DELTA, CONST_X0, CONST_DX0],
-            [float,float,float,float])
         self._forced = ParamPicker(
-            'Forced motion', [CONST_OMEGA, CONST_DELTA, CONST_X0, CONST_DX0, CONST_F],
-            [float,float,float,float,str])
-        self._tab_view.insertTab(0, self._harmonic, 'Harmonic')
-        self._tab_view.insertTab(1, self._damping, 'Damping')
-        self._tab_view.insertTab(2, self._forced, 'Forced')
+            'Motions',
+            [CONST_OMEGA, CONST_DELTA,CONST_X0, CONST_DX0, CONST_F],
+            ['1','0.01','0','0','2*cos(3*t+4)'],
+            [float, float, float, float, str]
+            )
+        self._tab_view.insertTab(0, self._forced, 'Forced')
         self._layout.addWidget(self._tab_view)
         self._graph_layout = QHBoxLayout()
-        self._graph_left = pg.PlotWidget()
+        self._graph_left = pg.PlotWidget(background=self._plot_background)
         self._graph_left.getPlotItem().addLine(x=0, pen=self._axis_pen)
         self._graph_left.getPlotItem().addLine(y=0, pen=self._axis_pen)
-        self._graph_right = pg.PlotWidget()
+        self._graph_right = pg.PlotWidget(background=self._plot_background)
         self._graph_right.getPlotItem().addLine(x=0, pen=self._axis_pen)
         self._graph_right.getPlotItem().addLine(y=0, pen=self._axis_pen)
         self._graph_layout.addWidget(self._graph_left)
@@ -111,19 +116,15 @@ class MainWindow(QWidget):
         except Exception as e:
             self._error_box.showMessage(str(e))
             return
-        if CONST_F in params:
-            expr = parse_expr(params[CONST_F])
-            f = make_f(lambda arg: expr.evalf(subs={t_symbol:arg}),[params[CONST_OMEGA]**2,2*params[CONST_DELTA]])
-        elif CONST_DELTA in params:
-            f = make_f(zero,[params[CONST_OMEGA]**2,2*params[CONST_DELTA]])
-        else:
-            f = make_f(zero,a = np.array([params[CONST_OMEGA]**2,0]))
-        x0 = [params[CONST_X0],params[CONST_DX0]]
-        x_res = odeint(f,x0,self._t).T
-        self.__update_plot__(self._t,x_res[0], 'left')
-        self.__update_plot__(x_res[0],x_res[1], 'right')
+        expr = parse_expr(params[CONST_F])
+        f = make_f(lambda arg: expr.evalf(subs={t_symbol: arg}), [
+                   params[CONST_OMEGA]**2, 2 * params[CONST_DELTA]])
+        x0 = [params[CONST_X0], params[CONST_DX0]]
+        x_res = odeint(f, x0, self._t).T
+        self.__update_plot__(self._t, x_res[0], 'left')
+        self.__update_plot__(x_res[0], x_res[1], 'right')
 
-    def __update_plot__(self,x,y, plot='left'):
+    def __update_plot__(self, x, y, plot='left'):
         if plot == 'left':
             plt = self._graph_left
         else:
@@ -131,7 +132,7 @@ class MainWindow(QWidget):
         plt.clear()
         plt.getPlotItem().addLine(x=0, pen=self._axis_pen)
         plt.getPlotItem().addLine(y=0, pen=self._axis_pen)
-        plt.addItem(pg.PlotDataItem(x,y))
+        plt.addItem(pg.PlotDataItem(x, y, pen=self._line_pen))
 
 
 if __name__ == '__main__':
